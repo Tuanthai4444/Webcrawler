f36d9a26e0c6fdc20d3740e826808835d94d4a04
tuant <tuanthai4444@gmail.com>
2020-11-28 23:18:56 -0800

    Fixed Calculator and Added Sorter Class

--- src/main/java/TFIDFCalculator.java
+++ src/main/java/TFIDFCalculator.java
@@ -10,97 +10,116 @@ public class TFIDFCalculator {
         counter = new WordCounter(texts);
     }
 
-    public double RegIDF(String word) {
-        double contains = 0;
-        List<Map<String, Integer>> map = counter.getAllDocsWC();
-        for(Map<String, Integer> d : map) {
-            if(d.containsKey(word)) {
-                contains++;
-            }
+
+    public class IDF {
+
+        double value;
+
+        public IDF() {
+            this.value = 0.0;
         }
 
-        double size = map.size();
-        return Math.log(size/contains);
-    }
-
-    public double smoothIDF(String word) {
-        double contains = 0;
-        List<Map<String, Integer>> map = counter.getAllDocsWC();
-        for(Map<String, Integer> d : map) {
-            if(d.containsKey(word)) {
-                contains++;
+        public void RegIDF(String word) {
+            double contains = 0;
+            List<Map<String, Integer>> map = counter.getAllDocsWC();
+            for(Map<String, Integer> d : map) {
+                if(d.containsKey(word)) {
+                    contains++;
+                }
             }
+
+            double size = map.size();
+            this.value = Math.log(size/contains);
         }
 
-        double size = map.size();
-        return Math.log(size/(contains+1)) + 1;
-    }
-
-    public double maxIDF(int selectedDoc, String word) {
-        double contains = 0;
-        List<Map<String, Integer>> list = counter.getAllDocsWC();
-        Map<String, Integer> map1 = list.get(selectedDoc);
-        double maxOccurTerm = Collections.max(map1.values());
-
-        List<Map<String, Integer>> map2 = counter.getAllDocsWC();
-        for(Map<String, Integer> d : map2) {
-            if(d.containsKey(word)) {
-                contains++;
+        public void smoothIDF(String word) {
+            double contains = 0;
+            List<Map<String, Integer>> map = counter.getAllDocsWC();
+            for(Map<String, Integer> d : map) {
+                if(d.containsKey(word)) {
+                    contains++;
+                }
             }
+
+            double size = map.size();
+            this.value = Math.log(size/(contains+1)) + 1;
         }
 
-        return Math.log(maxOccurTerm/(contains+1));
-    }
+        public void maxIDF(int selectedDoc, String word) {
+            double contains = 0;
+            List<Map<String, Integer>> list = counter.getAllDocsWC();
+            Map<String, Integer> map1 = list.get(selectedDoc);
+            double maxOccurTerm = Collections.max(map1.values());
 
-    public double probabilisticIDF(String word) {
-        double contains = 0;
-        List<Map<String, Integer>> map = counter.getAllDocsWC();
-        for(Map<String, Integer> d : map) {
-            if(d.containsKey(word)) {
-                contains++;
+            List<Map<String, Integer>> map2 = counter.getAllDocsWC();
+            for(Map<String, Integer> d : map2) {
+                if(d.containsKey(word)) {
+                    contains++;
+                }
             }
+
+            this.value = Math.log(maxOccurTerm/(contains+1));
         }
 
-        double size = map.size();
-        return Math.log((size-contains)/contains);
+        public void probabilisticIDF(String word) {
+            double contains = 0;
+            List<Map<String, Integer>> map = counter.getAllDocsWC();
+            for(Map<String, Integer> d : map) {
+                if(d.containsKey(word)) {
+                    contains++;
+                }
+            }
+
+            double size = map.size();
+            this.value = Math.log((size-contains)/contains);
+        }
     }
 
+    public class TF {
 
+        double value;
 
+        public TF() {
+            this.value = 0;
+        }
 
+        public double RetRawTF(int selectedDoc, String word) {
+            List<Map<String, Integer>> list = counter.getAllDocsWC();
+            Map<String, Integer> map = list.get(selectedDoc);
+            return (double)map.get(word);
+        }
 
-    public double RawTF(int selectedDoc, String word) {
-        List<Map<String, Integer>> list = counter.getAllDocsWC();
-        Map<String, Integer> map = list.get(selectedDoc);
-        return (double)map.get(word);
-    }
+        public void RawTF(int selectedDoc, String word) {
+            this.value = RetRawTF(selectedDoc, word);
+        }
 
-    public double RegTF(int selectedDoc, String word) {
-        List<String[]> list = counter.getAllDocsSplit();
-        String[] array = list.get(selectedDoc);
-        double value = array.length;
+        public void RegTF(int selectedDoc, String word) {
+            List<String[]> list = counter.getAllDocsSplit();
+            String[] array = list.get(selectedDoc);
+            double value = array.length;
 
-        return RawTF(selectedDoc, word)/value;
-    }
+            this.value = RetRawTF(selectedDoc, word)/value;
+        }
 
-    public double LogNormTF(int selectedDoc, String word) {
-        return Math.log(RawTF(selectedDoc, word) + 1);
-    }
+        public void LogNormTF(int selectedDoc, String word) {
+            this.value = Math.log(RetRawTF(selectedDoc, word) + 1);
+        }
 
-    public double doubleNormHalfValTF(int selectedDoc, String word) {
-        List<Map<String, Integer>> list = counter.getAllDocsWC();
-        Map<String, Integer> map = list.get(selectedDoc);
-        double maxOccurTerm = Collections.max(map.values());
+        public void doubleNormHalfValTF(int selectedDoc, String word) {
+            List<Map<String, Integer>> list = counter.getAllDocsWC();
+            Map<String, Integer> map = list.get(selectedDoc);
+            double maxOccurTerm = Collections.max(map.values());
 
-        return 0.5 + (0.5 * (RawTF(selectedDoc, word)/maxOccurTerm));
-    }
+            this.value = 0.5 + (0.5 * (RetRawTF(selectedDoc, word)/maxOccurTerm));
+        }
 
-    public double doubleNormKValTF(int selectedDoc, String word, double k) {
-        List<Map<String, Integer>> list = counter.getAllDocsWC();
-        Map<String, Integer> map = list.get(selectedDoc);
-        double maxOccurTerm = Collections.max(map.values());
+        public void doubleNormKValTF(int selectedDoc, String word, double k) {
+            List<Map<String, Integer>> list = counter.getAllDocsWC();
+            Map<String, Integer> map = list.get(selectedDoc);
+            double maxOccurTerm = Collections.max(map.values());
 
-        return k + (k * (RawTF(selectedDoc, word)/maxOccurTerm));
+            this.value = k + (k * (RetRawTF(selectedDoc, word)/maxOccurTerm));
+        }
     }
 
 }
--- src/main/java/TFIDFSorter.java
+++ src/main/java/TFIDFSorter.java
@@ -1,14 +1,2 @@
-import java.util.List;
-
 public class TFIDFSorter {
-
-    List<String> texts;
-    TFIDFCalculator calculator;
-
-    public TFIDFSorter(List<String> texts, TFIDFCalculator calculator) {
-        this.texts = texts;
-        this.calculator = calculator;
-    }
-
-
 }
